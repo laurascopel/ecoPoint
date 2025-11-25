2183d65c99df1518dda84d38767a905133879c14
Thais Pinheiro <seuemail@email.com>
2025-11-25 11:28:40 -0300

    Criação do repositório

---
+++ src/main/java/grupo2/com/ecoPoint/Model/Entity/EmpresaEnvia.java
@@ -0,0 +1,61 @@
+package grupo2.com.ecoPoint.Model.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class EmpresaEnvia {
+@Id
+private Integer id;
+private String nome;
+private String cnpj;
+private String endereco;
+private String telefone;
+  
+public EmpresaEnvia() {
+}
+
+public EmpresaEnvia(Integer id, String nome, String cnpj, String endereco, String telefone) {
+    this.id = id;
+    this.nome = nome;
+    this.cnpj = cnpj;
+    this.endereco = endereco;
+    this.telefone = telefone;
+    }   
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+ public String getNome() {
+      return nome;
+  }
+    public void setNome(String nome) {
+      this.nome = nome;
+  }
+
+   public String getCnpj() {
+      return cnpj;
+  }
+    public void setCnpj(String cnpj) {
+      this.cnpj = cnpj;
+  }
+
+  public String getEndereco() {
+      return endereco;
+  }
+    public void setEndereco(String endereco) {
+      this.endereco = endereco;
+  }
+
+  public String getTelefone() {
+      return telefone;
+  }
+    public void setTelefone(String telefone) {
+      this.telefone = telefone;
+  }
+
+}
---
+++ src/main/java/grupo2/com/ecoPoint/Model/Entity/EmpresaRecebe.java
@@ -0,0 +1,79 @@
+package grupo2.com.ecoPoint.Model.Entity;
+
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class EmpresaRecebe {
+    @Id
+    private Integer id;
+    private String nome;
+    private String endereco;
+    private LocalTime horarioFuncionamento;
+    private String telefone;
+    private String descricao;
+    private boolean anexaDoc;
+  
+    public EmpresaRecebe() {
+    }
+
+    public EmpresaRecebe(Integer id, String nome, String endereco, LocalTime horarioFuncionamento, String telefone, String descricao, Boolean anexaDoc) {
+    this.id = id;
+    this.nome = nome;
+    this.endereco = endereco;
+    this.horarioFuncionamento = horarioFuncionamento;
+    this.telefone = telefone;
+    this.descricao = descricao;
+    this.anexaDoc = anexaDoc;
+    }
+
+    public Integer getId() {
+      return id;
+  }
+    public void setId(Integer id) {
+      this.id = id;
+  }
+
+    public String getNome() {
+      return nome;
+  }
+    public void setNome(String nome) {
+      this.nome = nome;
+  }
+  
+    public String getEndereco() {
+      return endereco;
+  }
+    public void setEndereco(String endereco) {
+      this.endereco = endereco;
+  }
+
+      public LocalTime getHorarioFuncionamento() {
+      return horarioFuncionamento;
+  }
+    public void setHorarioFuncionamento(LocalTime horarioFuncionamento) {
+      this.horarioFuncionamento = horarioFuncionamento;
+  }
+
+    public String getTelefone() {
+      return telefone;
+  }
+    public void setTelefone(String telefone) {
+      this.telefone = telefone;
+  }
+
+    public String getDescricao() {
+      return descricao;
+  }
+    public void setDescricao(String descricao) {
+      this.descricao = descricao;
+  }
+    public Boolean getAnexaDoc() {
+      return anexaDoc;
+  }
+    public void setAnexaDoc(Boolean anexaDoc) {
+      this.anexaDoc = anexaDoc;
+  }
+}
